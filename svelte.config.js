c6b2097342411414359dada6386a328512040e35
Dwi-Wahyu <dwiwahyuilahi123@gmail.com>
2026-04-08 20:19:54 +0800

    fix configuration

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,11 @@
 // import adapter from '@sveltejs/adapter-auto';
 // import adapter from '@sveltejs/adapter-node';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import adapter from 'svelte-adapter-bun';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter(),
 		alias: {
